60343b9f51598349917b7105124b797a11a63322
Kyle Ellman <kyle.ellman@gmail.com>
2010-03-24 16:03:21 -0500

    testing renaming files

---
+++ system/application/controllers/user.php
@@ -0,0 +1,17 @@
+<?php
+
+class User extends Controller {
+
+	function User()
+	{
+		parent::Controller();	
+	}
+	
+	function index()
+	{
+		$this->load->view('welcome_message');
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./system/application/controllers/welcome.php */
