9d9d1b463bec896b10f22c7faa283728c09fdee3
Michael Wisely <michaelwisely@gmail.com>
2010-04-28 23:14:09 -0500

    fixed a little syntax error

--- system/application/models/media_model.php
+++ system/application/models/media_model.php
@@ -14,7 +14,10 @@ Class Media_model extends Query
 		
 		$user_id = $CI->user_model->currentUser();
 		
-		$this->query->addMedia($user_id, $attr['genre'], $attr['title'], $attr['type'], $attr['author'], $attr['publisher'], $attr['ISBN'], $attr['writer'], $attr['writer'], $attr['director']);
+		$this->query->addMedia($user_id, $attr['genre'], $attr['title'],
+				       $attr['type'], $attr['author'],
+				       $attr['publisher'], $attr['ISBN'],
+				       $attr['writer'], $attr['writer'], $attr['director']);
 	}
 	
 	function search($search)
@@ -26,7 +29,7 @@ Class Media_model extends Query
 	
 	function modify_media($attr)
 	{
-		$this->query->modify_media($attr['media_id'], $attr['user_id'], $attr['genre', 
+		$this->query->modify_media($attr['media_id'], $attr['user_id'], $attr['genre'], 
 					   $attr['title'], $attr['author'], $attr['publisher'], 
 					   $attr['ISBN'], $attr['artist'], $attr['writer'],
 			      		   $attr['director']);
