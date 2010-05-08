0687bfa2a053357c1991d090e95eb792386ca028
Kyle Ellman <kyle.ellman@gmail.com>
2010-05-07 20:20:10 -0500

    request friendship link or this is you text on profile depending on if it is you or someone else

--- system/application/views/profile.php
+++ system/application/views/profile.php
@@ -8,6 +8,14 @@
 	</div>
 	
 	<div id="main">
+		<h1 style="margin-bottom:0"><?=$userData['fname']?> <?=$userData['lname']?></h1>
+		<h3 style="margin-bottom:0"><?=$userData['user_id']?></h3>
+		<?php if($this->session->userdata('user_id') == $userData['user_id']): ?>
+			<p>This is you!</p>
+		<?php else: ?>
+			<p><?=anchor('friend/request/'.$userData['user_id'], 'Request friendship')?></p>
+		<?php endif; ?>
+		<br />
 		<h2><?=$userData['fname']?>'s media</h2><br /><br />
 		<?=$this->load->view('media_list', array('profile' => true))?>
 	</div>
